78484c4a9bb98281add7cd9e6826bb2d2e7fecdf
Karen <k.ortiz026@gmail.com>
2019-05-07 19:58:33 -0400

    Grabbed id's for each diet restriction selected
    - Display restaurants using JS.
    - Added code that will demonstrate what checkboxes were selected.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Array of restaurants
 const restaurants = [
   {
     name: "Dig Inn - Midtown South",
@@ -13,19 +14,19 @@ const restaurants = [
         name: "Roasted Wild Alaskan Salmon Marketbowl",
         description: "Wild Alaskan Salmon with lemon thyme. Gluten-Free. Farm Greens with Mint (Base), Cashew Kale Caesar (Market side) , Lemon Wedge (Sauce)",
         price: 12.96,
-        tags: ["Low Sodium", "Low Sugar"]
+        tags: ["low sodium", "low sugar"]
       },
       {
         name: "Charred Chicken Market Bowl",
         description: "Antibiotic-free chicken thigh with lemon, fennel, and mustard seeds. Gluten-Free. Farm Greens with Mint (Base), Lemon Wedge (Sauce)",
         price: 11.62,
-        tags: ["Low Sodium"]
+        tags: ["low sodium"]
       },
       {
         name: "Organic Iced Tea",
         description: "Brew Lab organic iced teas",
         price: 2.75,
-        tags: ["Low Sugar"]
+        tags: ["low sugar"]
       }
     ] //end of menu items
   }, //end of restaurant 1 - Dig Inn - Midtown South
@@ -43,20 +44,20 @@ const restaurants = [
         name: "Charred Cauliflower",
         description: "Maitake mushroom, market herbs, apple, almond, green curry",
         price: 22.00,
-        tags: ["Low Sugar"]
+        tags: ["low sugar"]
       },
       {
         name: "Charred Cauliflower",
         description: "Maitake mushroom, market herbs, apple, almonds, green curry.",
         specialInstructions: "Ask for no almonds",
         price: 22.00,
-        tags: ["Low Potassium"]
+        tags: ["low potassium"]
       },
       {
         name: "Cauliflower & Mushroom tacos",
         description: "oaxacan mole, red cabbage, black bean, avocado, cilantro ",
         price: 17.00,
-        tags: ["Low Sugar"]
+        tags: ["low sugar"]
       }
     ] //end of menu items
   }, //end of restaurant 2 - The Little Beet Table
@@ -74,7 +75,7 @@ const restaurants = [
         name: "Tuna & Hummus Wrap",
         description: "A scoop of low-sodium tuna mixed with a scoop of hummus with oil and vinegar in a whole wheat wrap. Comes with Romaine, cucumbers and tomatoes.",
         price: 8.75,
-        tags: ["Low Sugar"]
+        tags: ["low sugar"]
       }
     ] //end of menu items
   }, //end of restaurant 3 - Natureworks Restaurant - E 31st
@@ -92,7 +93,7 @@ const restaurants = [
         name: "Heirloom Cauliflower",
         description: "Features a trio of colorful cauliflower (white, purple, marigold), which are roasted in brown butter and sage until the floret coats turn a golden-brown. The cauliflower is also tossed with juice from the pickled pears to add a little more zing, and for crunch",
         price: 9.00,
-        tags: ["Low Potassium"]
+        tags: ["low potassium"]
       }
     ] //end of menu items
   }, //end of restaurant 4 - The Dutch
@@ -111,19 +112,19 @@ const restaurants = [
         description: "Walnuts, diced apples, berries and brown sugar.",
         specialInstructions:"No Walnuts",
         price: 10.00,
-        tags: ["Low Potassium"]
+        tags: ["low potassium"]
       },
       {
         name: "Build your own Omelette",
         description: "Egg whites, onions, peppers, rye toast and side fresh fruit",
         price: 14.00,
-        tags: ["Low Potassium"]
+        tags: ["low potassium"]
       },
       {
         name: "Pollo Scarpariello",
         description: "Chicken sauteed with garlic, lemon and white wine.",
         price: 26.00,
-        tags: ["Low Potassium"]
+        tags: ["low potassium"]
       }
     ] //end of menu items
   }, //end of restaurant 5 - Allora
@@ -141,20 +142,20 @@ const restaurants = [
         name: "Green Salad",
         description: "Granny Smith apple, Manchego, radish, almonds",
         price: 12.00,
-        tags: ["Low Potassium"]
+        tags: ["low potassium"]
       },
       {
         name: "Bone-In Sirlion",
         description: "Grilled over hickory logs.",
         specialInstructions: "Ask for no almonds",
         price: 42.00,
-        tags: ["Low Potassium"]
+        tags: ["low potassium"]
       },
       {
         name: "Steamed Green Beans",
         description: "",
         price: 12.00,
-        tags: ["Low Potassium"]
+        tags: ["low potassium"]
       }
     ] //end of menu items
   }, //end of restaurant 6 - Gallagher Steakhouse
@@ -172,7 +173,7 @@ const restaurants = [
         name: "Scorched Cauliflower",
         description: "toasted garlic, fried capers, cauliflower puree.",
         price: 18.00,
-        tags: ["Low Potassium"]
+        tags: ["low potassium"]
       }
     ] //end of menu items
   }, //end of restaurant 7 - Avant Garden - East Village
@@ -190,13 +191,13 @@ const restaurants = [
         name: "Arugula Salad",
         description: "with apple, celery and pistacchio",
         price: 13.00,
-        tags: ["Low Potassium","Low Sugar"]
+        tags: ["low potassium","low sugar"]
       },
       {
         name: "Melanzane alla Parmigiana",
         description: "with saffron fonduta & basil breadcrumbs.",
         price: 19.00,
-        tags: ["Low Potassium", "Low Sugar"]
+        tags: ["low potassium", "low sugar"]
       }
     ] //end of menu items
   }, //end of restaurant 8 - Adelina's
@@ -204,15 +205,36 @@ const restaurants = [
 
 $(function() {
   console.log("Ready!");
-  // $.getJSON( "./js/restaurants.json", function( data ) {
-  //   console.log( data ); // Data returned
-  // });
 
+  // print restaurant array to console.
   console.log(restaurants);
+
+  //Set up an empty array that will store the selected diet restrictions
+  let selectedRestrictions = [];
+
+  //Print list of restaurants to the landing page.
   $.each(restaurants, function(index,value){
-    console.log("index:", index);
-    console.log("restaurant name:", value.name);
-    console.log("restaurant address:", value.address.street);
+    // console.log("index:", index);
+    // console.log("restaurant name:", value.name);
+    // console.log("restaurant address:", value.address.street);
+    $(".restaurant-list ul").append(`<li><a href="resultspage.html">${value.name}</a></li>`);
   });
+
+  //Listen for a click on the search button and call function handle
+  //handleFilter to handle the filter.
+  $(".search-button").click(handleFilter);
+
+  //Fucntion to handle filters that were selected by user.
+  function handleFilter (){
+    console.log("clicked filter");
+    $(".checkbox:checked").each(function(index, checkbox) {
+      console.log('selected item', checkbox.id)
+      selectedRestrictions.push(checkbox.id)
+      console.log(selectedRestrictions);
+    })
+  };
+
+  //Function that will remove a dash from checkbox id. 
+
 
 });
