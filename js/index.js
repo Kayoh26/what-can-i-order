2ea858994accc43885aae311c8932a6156dc471c
Karen <k.ortiz026@gmail.com>
2019-05-08 08:48:48 -0400

    Added special insturctions property to restuarants.

--- js/index.js
+++ js/index.js
@@ -13,18 +13,21 @@ const restaurants = [
       {
         name: "Roasted Wild Alaskan Salmon Marketbowl",
         description: "Wild Alaskan Salmon with lemon thyme. Gluten-Free. Farm Greens with Mint (Base), Cashew Kale Caesar (Market side) , Lemon Wedge (Sauce)",
+        specialInstructions: "",
         price: 12.96,
         tags: ["low sodium", "low sugar"]
       },
       {
         name: "Charred Chicken Market Bowl",
         description: "Antibiotic-free chicken thigh with lemon, fennel, and mustard seeds. Gluten-Free. Farm Greens with Mint (Base), Lemon Wedge (Sauce)",
+        specialInstructions: "",
         price: 11.62,
         tags: ["low sodium"]
       },
       {
         name: "Organic Iced Tea",
         description: "Brew Lab organic iced teas",
+        specialInstructions: "",
         price: 2.75,
         tags: ["low sugar"]
       }
@@ -43,6 +46,7 @@ const restaurants = [
       {
         name: "Charred Cauliflower",
         description: "Maitake mushroom, market herbs, apple, almond, green curry",
+        specialInstructions: "",
         price: 22.00,
         tags: ["low sugar"]
       },
@@ -56,6 +60,7 @@ const restaurants = [
       {
         name: "Cauliflower & Mushroom tacos",
         description: "oaxacan mole, red cabbage, black bean, avocado, cilantro ",
+        specialInstructions: "",
         price: 17.00,
         tags: ["low sugar"]
       }
@@ -74,6 +79,7 @@ const restaurants = [
       {
         name: "Tuna & Hummus Wrap",
         description: "A scoop of low-sodium tuna mixed with a scoop of hummus with oil and vinegar in a whole wheat wrap. Comes with Romaine, cucumbers and tomatoes.",
+        specialInstructions: "",
         price: 8.75,
         tags: ["low sugar"]
       }
@@ -92,6 +98,7 @@ const restaurants = [
       {
         name: "Heirloom Cauliflower",
         description: "Features a trio of colorful cauliflower (white, purple, marigold), which are roasted in brown butter and sage until the floret coats turn a golden-brown. The cauliflower is also tossed with juice from the pickled pears to add a little more zing, and for crunch",
+        specialInstructions: "",
         price: 9.00,
         tags: ["low potassium"]
       }
@@ -117,12 +124,14 @@ const restaurants = [
       {
         name: "Build your own Omelette",
         description: "Egg whites, onions, peppers, rye toast and side fresh fruit",
+        specialInstructions: "",
         price: 14.00,
         tags: ["low potassium"]
       },
       {
         name: "Pollo Scarpariello",
         description: "Chicken sauteed with garlic, lemon and white wine.",
+        specialInstructions: "",
         price: 26.00,
         tags: ["low potassium"]
       }
@@ -141,6 +150,7 @@ const restaurants = [
       {
         name: "Green Salad",
         description: "Granny Smith apple, Manchego, radish, almonds",
+        specialInstructions: "",
         price: 12.00,
         tags: ["low potassium"]
       },
@@ -154,6 +164,7 @@ const restaurants = [
       {
         name: "Steamed Green Beans",
         description: "",
+        specialInstructions: "",
         price: 12.00,
         tags: ["low potassium"]
       }
@@ -172,6 +183,7 @@ const restaurants = [
       {
         name: "Scorched Cauliflower",
         description: "toasted garlic, fried capers, cauliflower puree.",
+        specialInstructions: "",
         price: 18.00,
         tags: ["low potassium"]
       }
@@ -190,12 +202,14 @@ const restaurants = [
       {
         name: "Arugula Salad",
         description: "with apple, celery and pistacchio",
+        specialInstructions: "",
         price: 13.00,
         tags: ["low potassium","low sugar"]
       },
       {
         name: "Melanzane alla Parmigiana",
         description: "with saffron fonduta & basil breadcrumbs.",
+        specialInstructions: "",
         price: 19.00,
         tags: ["low potassium", "low sugar"]
       }
@@ -219,18 +233,20 @@ $(function() {
   //iterate through the restaurants array of objects
   //Get array of tags
   $.each(restaurants,function (index,value) {
-    console.log(value.name, value.menuItems);
+    console.log(`Restaurant ${index+1} Name:`,value.name, value.menuItems);
     console.log('Is menuItems an array?:',$.isArray(value.menuItems));
     //Iterate through menuItems array of objects
     $.each(value.menuItems, function (index2,value2) {
       console.log(index2, value2.tags);
-      console.log($.isArray(value2.tags));
+      console.log(`Menu Item ${index2+1}:`,value2.name);
+      console.log('Is menu Item tags an array?',$.isArray(value2.tags));
+      
       //Iterate through tags array of menu item
       $.each(value2.tags,function (index3,value3) {
-        console.log("Menu item Tag: " ,value3);
+        console.log(`Menu item ${index2+1} Tag ${index3+1}:` ,value3);
         console.log("userRestriction: ", userRestriction)
         console.log("Item tag and user restriction match =" , value3 === userRestriction);
-        console.log("Restaurant Name:", value.name);
+        // console.log("Restaurant Name:", value.name);
       });
     });
   });
--- js/restaurants.js
+++ js/restaurants.js
@@ -12,18 +12,21 @@ const restaurants = [
       {
         name: "Roasted Wild Alaskan Salmon Marketbowl",
         description: "Wild Alaskan Salmon with lemon thyme. Gluten-Free. Farm Greens with Mint (Base), Cashew Kale Caesar (Market side) , Lemon Wedge (Sauce)",
+        specialInstructions: "",
         price: 12.96,
         tags: ["low Sodium", "low Sugar"]
       },
       {
         name: "Charred Chicken Market Bowl",
         description: "Antibiotic-free chicken thigh with lemon, fennel, and mustard seeds. Gluten-Free. Farm Greens with Mint (Base), Lemon Wedge (Sauce)",
+        specialInstructions: "",
         price: 11.62,
         tags: ["low sodium"]
       },
       {
         name: "Organic Iced Tea",
         description: "Brew Lab organic iced teas",
+        specialInstructions: "",
         price: 2.75,
         tags: ["low sugar"]
       }
@@ -42,6 +45,7 @@ const restaurants = [
       {
         name: "Charred Cauliflower",
         description: "Maitake mushroom, market herbs, apple, almond, green curry",
+        specialInstructions: "",
         price: 22.00,
         tags: ["low sugar"]
       },
@@ -55,6 +59,7 @@ const restaurants = [
       {
         name: "Cauliflower & Mushroom tacos",
         description: "oaxacan mole, red cabbage, black bean, avocado, cilantro ",
+        specialInstructions: "",
         price: 17.00,
         tags: ["low sugar"]
       }
@@ -73,6 +78,7 @@ const restaurants = [
       {
         name: "Tuna & Hummus Wrap",
         description: "A scoop of low-sodium tuna mixed with a scoop of hummus with oil and vinegar in a whole wheat wrap. Comes with Romaine, cucumbers and tomatoes.",
+        specialInstructions: "",
         price: 8.75,
         tags: ["low sugar"]
       }
@@ -91,6 +97,7 @@ const restaurants = [
       {
         name: "Heirloom Cauliflower",
         description: "Features a trio of colorful cauliflower (white, purple, marigold), which are roasted in brown butter and sage until the floret coats turn a golden-brown. The cauliflower is also tossed with juice from the pickled pears to add a little more zing, and for crunch",
+        specialInstructions: "",
         price: 9.00,
         tags: ["low potassium"]
       }
@@ -116,12 +123,14 @@ const restaurants = [
       {
         name: "Build your own Omelette",
         description: "Egg whites, onions, peppers, rye toast and side fresh fruit",
+        specialInstructions: "",
         price: 14.00,
         tags: ["low potassium"]
       },
       {
         name: "Pollo Scarpariello",
         description: "Chicken sauteed with garlic, lemon and white wine.",
+        specialInstructions: "",
         price: 26.00,
         tags: ["low potassium"]
       }
@@ -140,6 +149,7 @@ const restaurants = [
       {
         name: "Green Salad",
         description: "Granny Smith apple, Manchego, radish, almonds",
+        specialInstructions: "",
         price: 12.00,
         tags: ["low potassium"]
       },
@@ -153,6 +163,7 @@ const restaurants = [
       {
         name: "Steamed Green Beans",
         description: "",
+        specialInstructions: "",
         price: 12.00,
         tags: ["low potassium"]
       }
@@ -171,6 +182,7 @@ const restaurants = [
       {
         name: "Scorched Cauliflower",
         description: "toasted garlic, fried capers, cauliflower puree.",
+        specialInstructions: "",
         price: 18.00,
         tags: ["low potassium"]
       }
@@ -189,12 +201,14 @@ const restaurants = [
       {
         name: "Arugula Salad",
         description: "with apple, celery and pistacchio",
+        specialInstructions: "",
         price: 13.00,
         tags: ["low potassium","low sugar"]
       },
       {
         name: "Melanzane alla Parmigiana",
         description: "with saffron fonduta & basil breadcrumbs.",
+        specialInstructions: "",
         price: 19.00,
         tags: ["low potassium", "low sugar"]
       }
